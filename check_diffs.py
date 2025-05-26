7ff85788fd158113794c66497191e61fcd94de9c
Nils Podewitz <nils.podewitz@costamarebulkers.com>
2025-05-26 12:05:17 +0200

    Added logic

--- check_diffs.py
+++ check_diffs.py
@@ -3,5 +3,21 @@ from git import Repo
 
 repo = Repo(".")
 
-for diff in repo.head.commit.diff("main"):
-    print(diff.a_path)
+changed_files = [diff.a_path for diff in repo.head.commit.diff(None)]
+# for diff in repo.head.commit.diff(None):
+#     print(diff.a_path)
+
+tables_to_check = {}
+for changed_file in changed_files:
+    print(f"Changed file: {changed_file}")
+    # 1. Extract output table name(s)
+    # 2. Get last access time of table for "normal" users using "ODBC Cluster"
+    # 3. Add to list if applicable
+
+# 3. Check if any table was changed that could affect users and raise Exception if so
+if tables_to_check:
+    formated_tables = [f"{table}: {users}" for table, users in tables_to_check.items()]
+    raise Exception(
+        "The following tables were changed that may affect users:"
+        + "\n  * ".join(formated_tables)
+    )
