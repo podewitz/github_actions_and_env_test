d10c8692f6f04c57bfdec31f346b505a7edea6d1
Nils Podewitz <nils.podewitz@costamarebulkers.com>
2025-05-27 10:06:54 +0200

    Added comment on changed files

--- check_diffs.py
+++ check_diffs.py
@@ -1,4 +1,7 @@
+import os
+
 from git import Repo
+from github import Auth, Github
 
 
 repo = Repo(".")
@@ -13,11 +16,38 @@ for changed_file in changed_files:
     # 1. Extract output table name(s)
     # 2. Get last access time of table for "normal" users using "ODBC Cluster"
     # 3. Add to list if applicable
+    tables_to_check[changed_file] = []
 
 # 3. Check if any table was changed that could affect users and raise Exception if so
-if tables_to_check:
-    formated_tables = [f"{table}: {users}" for table, users in tables_to_check.items()]
-    raise Exception(
-        "The following tables were changed that may affect users:"
-        + "\n  * ".join(formated_tables)
+# if tables_to_check:
+#     formated_tables = [f"{table}: {users}" for table, users in tables_to_check.items()]
+#     raise Exception(
+#         "The following tables were changed that may affect users:"
+#         + "\n  * ".join(formated_tables)
+#     )
+
+token = os.getenv("GITHUB_TOKEN")
+print(f"Using token: {token=}")
+
+# Public Web Github
+g = Github(auth=Auth.Token(token))
+
+full_repo_name = os.getenv("GITHUB_REPO")
+print(f"Using repo: {full_repo_name=}")
+repo = g.get_repo(full_repo_name)
+
+pull_number = int(os.getenv("GITHUB_PULL_NUMBER"))
+print(f"Using pull number: {pull_number=}")
+pull_request = repo.get_pull(number=pull_number)
+
+for file, users in tables_to_check.items():
+    print(f"File: {file}, Users: {users}")
+    pull_request.create_review_comment(
+        body=f"This is a test for {file} with {users}",
+        commit=pull_request.head.sha,
+        path=file,
+        subject_type="file",
     )
+
+# To close connections after use
+g.close()
